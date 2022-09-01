4ca17b5d2dcf5c776f52b5fb274f7114858c8463
ki-chi <k.brilliant@gmail.com>
2022-09-02 02:11:14 +0900

    feat: impl get, post, and authorize

--- src/JQuants.jl
+++ src/JQuants.jl
@@ -1,24 +1,85 @@
 module JQuants
 
+using HTTP
+using JSON
+
 const REFRESH_TOKEN = Ref{String}()
 const ID_TOKEN = Ref{String}()
+const JPX_URL = "https://api.jpx-jquants.com/v1"
+
+@enum EndPointKey begin
+    TokenAuthUser
+    TokenAuthRefresh
+    ListedInfo
+    ListedSections
+    PricesDailyQuotes
+    FinsStatements
+end
+
+const endpoints = Base.ImmutableDict(
+    TokenAuthUser => "/token/auth_user",
+    TokenAuthRefresh => "/token/auth_refresh",
+    ListedInfo => "/listed/info",
+    ListedSections => "/listed/sections",
+    PricesDailyQuotes => "/prices/daily_quotes",
+    FinsStatements => "/fins/statements"
+)
+
+
 
 isvalid_auth() = isdefined(REFRESH_TOKEN, 1) && isdefined(ID_TOKEN, 1) 
 
 function update_ref_token(mailaddress, password)
-    # todo
+    body = JSON.json(Dict("mailaddress"=>mailaddress, "password"=>password))
+    resp = post(JQuants.TokenAuthUser, body=body)
+    REFRESH_TOKEN[] = resp["refreshToken"]
 end
 
 function update_id_token()
-    # todo
+    resp = post(JQuants.TokenAuthRefresh, query=["refreshtoken"=>REFRESH_TOKEN[]])
+    ID_TOKEN[] = resp["idToken"]
 end
 
 function authorize(refresh_token)
     REFRESH_TOKEN[] = refresh_token
-    ID_TOKEN[] = getidtoken()
+    update_id_token()
+    return isvalid_auth()
 end
 
 function authorize(mailaddress, password)
-    REFRESH_TOKEN[] = update_ref_token(mailaddress, password)
-    ID_TOKEN[] = getidtoken()
+    update_ref_token(mailaddress, password)
+    update_id_token()
+    return isvalid_auth()
 end
+
+function get(endpointkey::EndPointKey, query)
+    endpoint = JPX_URL * endpoints[endpointkey]
+    headers = ["Authorization" => "Bearer $(ID_TOKEN[])"]
+    resp = HTTP.get(endpoint, headers=headers, query=query)
+    body = JSON.parse(String(resp.body))
+
+    if resp.status != 200
+        statustext = HTTP.Messages.statustext(resp.status)
+        message = body["message"]
+        error("Status: $(resp.status) $(statustext)\n Message: $(message)")
+    end
+
+    return body
+end
+
+function post(endpointkey::EndPointKey; kwargs...)
+    endpoint = JPX_URL * endpoints[endpointkey]
+    resp = HTTP.post(endpoint; kwargs...)
+    body = JSON.parse(String(resp.body))
+
+    if resp.status != 200
+        statustext = HTTP.Messages.statustext(resp.status)
+        message = body["message"]
+        error("Status: $(resp.status) $(statustext)\n Message: $(message)")
+    end
+
+    return body
+end
+
+
+end # module
