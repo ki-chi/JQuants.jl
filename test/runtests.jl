835fc5bab2b271057af9bf7175047977d8478e97
ki-chi <k.brilliant@gmail.com>
2022-09-03 01:22:32 +0900

    test: update tests for authorize and getlistedinfo

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,21 +2,21 @@ using JQuants
 using Test
 
 @testset "Authorization" begin
-    JQuants.authorize(refresh_token)
-    JQuants.authorize(mail_address, password)
+    refresh_token = ENV["JQUANTS_REFRESH_TOKEN"]
+    mail_address = ENV["JQUANTS_MAIL_ADDRESS"]
+    password = ENV["JQUANTS_PASSWORD"]
+    @test JQuants.authorize(refresh_token)
+    @test JQuants.authorize(mail_address, password)
 end
 
 @testset "Inquiry of listed issues information" begin
-    issue_code = "86970"  # JPX
-    listed_info = getlistedinfo(issue_code)
+    code = "86970"  # JPX
+    listed_info = getlistedinfo(code=code)
 
-    @test listed_info.code == "86970"
-    @test listed_info.company_name == "ＪＰＸ"
-    @test listed_info.company_name_english == "Japan Exchange Group,Inc."
-    @test listed_info.company_name_full == "(株) 日本取引所グループ"
-    @test listed_info.sector_code == "7200"
-    @test listed_info.market_code == "A"
-
-    listed_sections = getlistedsections()
-    
+    @test listed_info[begin, :Code] == "86970"
+    @test listed_info[begin, :CompanyName] == "ＪＰＸ"
+    @test listed_info[begin, :CompanyNameEnglish] == "Japan Exchange Group,Inc."
+    @test listed_info[begin, :CompanyNameFull] == "（株）日本取引所グループ"
+    @test listed_info[begin, :MarketCode] == "A"
+    @test listed_info[begin, :SectorCode] == "7200"
 end
