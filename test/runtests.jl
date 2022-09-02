fd9b9e5fec074bb2f4dec9a366560a8ea5e835cf
ki-chi <k.brilliant@gmail.com>
2022-09-03 02:19:11 +0900

    test: add test for getsections and getdailyquotes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using JQuants
+using DataFrames
 using Test
 
 @testset "Authorization" begin
@@ -9,9 +10,9 @@ using Test
     @test JQuants.authorize(mail_address, password)
 end
 
-@testset "Inquiry of listed issues information" begin
+@testset "Listed issues information" begin
     code = "86970"  # JPX
-    listed_info = getlistedinfo(code=code)
+    listed_info = getinfo(code=code)
 
     @test listed_info[begin, :Code] == "86970"
     @test listed_info[begin, :CompanyName] == "ＪＰＸ"
@@ -19,4 +20,31 @@ end
     @test listed_info[begin, :CompanyNameFull] == "（株）日本取引所グループ"
     @test listed_info[begin, :MarketCode] == "A"
     @test listed_info[begin, :SectorCode] == "7200"
+end
+
+@testset "Listed sections" begin
+    listed_sections = getsections()
+    
+    @test size(listed_sections) == (34, 2)
+    @test subset(listed_sections, :SectorCode => x -> x .== "7150")[begin, :SectorName] == "保険業"
+    @test subset(listed_sections, :SectorCode => x -> x .== "9999")[begin, :SectorName] == "(その他)"
+end
+
+@testset "Daily prices" begin
+    one_code_all_daily_quotes = getdailyquotes(code="86970")
+    expected_colnames = [
+        "AdjustmentClose", "AdjustmentFactor",
+        "AdjustmentHigh", "AdjustmentLow", "AdjustmentOpen", "AdjustmentVolume",
+        "Close", "Code", "Date", "High", "Low", "Open", "TurnoverValue", "Volume"
+    ]
+    expected_coltypes = [
+        Union{Nothing, Float64}, Float64,
+        Union{Nothing, Float64}, Union{Nothing, Float64}, Union{Nothing, Float64},
+        Union{Nothing, Float64}, Union{Nothing, Float64}, String, String,
+        Union{Nothing, Float64}, Union{Nothing, Float64}, Union{Nothing, Float64},
+        Union{Nothing, Float64}, Union{Nothing, Float64}
+    ]
+
+    @test names(one_code_all_daily_quotes) == expected_colnames
+    @test eltype.(eachcol(one_code_all_daily_quotes)) == expected_coltypes
 end
