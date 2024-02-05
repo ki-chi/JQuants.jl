f6d23aac43790e7b314a82b8a69e0d961e3ed0cd
ki-chi <k.brilliant@gmail.com>
2024-02-05 13:29:09 +0900

    test: fix dates in some test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,10 +13,24 @@ end
     @test JQuants.authorize(emailaddress, password)
 end
 
+@testset "Trading Calendar" begin
+    calendar = fetch(TradingCalendar(holidaydivision="1"))
+
+    expected_colnames = [
+        "Date",
+        "HolidayDivision",
+    ]
+
+    @test sort(names(calendar)) == sort(expected_colnames)
+end
+
+# Get the latest date from the trading calendar
+test_date = fetch(TradingCalendar(holidaydivision="1")) |> last |> row -> row[:Date]
+test_holiday = fetch(TradingCalendar(holidaydivision="0")) |> last |> row -> row[:Date]
+
 @testset "Listed issues information" begin
     code = "86970"  # JPX
-    date = today() - Month(3)  # 3 months ago (because the available periods for free plan is limited)
-    listed_info = fetch(ListedInfo(code=code, date=date))
+    listed_info = fetch(ListedInfo(code=code, date=test_date))
 
     expected_colnames = [
         "Date", "Code", "CompanyName", "CompanyNameEnglish", "Sector17Code",
@@ -40,8 +54,7 @@ end
 end
 
 @testset "Daily prices" begin
-    date = today() - Month(3)
-    daily_quotes = fetch(PricesDailyQuotes(date=date))
+    daily_quotes = fetch(PricesDailyQuotes(date=test_date))
     expected_colnames = [
         "AdjustmentClose", "AdjustmentFactor",
         "AdjustmentHigh", "AdjustmentLow", "AdjustmentOpen", "AdjustmentVolume",
@@ -63,7 +76,7 @@ end
     end
 
     # No output on a holiday
-    daily_quotes_null = fetch(PricesDailyQuotes(date="2023-01-01"))
+    daily_quotes_null = fetch(PricesDailyQuotes(date=test_holiday))
     @test isempty(daily_quotes_null)
     @test daily_quotes_null == Any[]
 end
@@ -92,17 +105,4 @@ end
     ]
 
     @test sort(names(ann)) == sort(expected_colnames)
-end
-
-@testset "Trading Calendar" begin
-    calendar = fetch(TradingCalendar(
-        holidaydivision="1",
-        from="2023-01-01", to="2023-01-31"))
-
-    expected_colnames = [
-        "Date",
-        "HolidayDivision",
-    ]
-
-    @test sort(names(calendar)) == sort(expected_colnames)
 end
